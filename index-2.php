12ad5ecfdc5f9df84a98abfcb4f1c116d38b4dca
Alessandro Marega <ale_marega@hotmail.it>
2021-05-04 17:01:39 +0200

    aggiunto controllo immissione query string

--- index-2.php
+++ index-2.php
@@ -4,10 +4,14 @@
   $mail = $_GET["mail"];
   $age = $_GET["age"];
 
-  if (strlen($name) > 3 && strpos($mail, ".") && strpos($mail, "@") && is_numeric($age)) {
-    echo "Accesso Riuscito";
+  if ($name != "" && $mail != "" && $age != "") {
+    if (strlen($name) > 3 && strpos($mail, ".") && strpos($mail, "@") && is_numeric($age)) {
+      echo "Accesso Riuscito";
+    } else {
+      echo "Accesso Negato";
+    }
   } else {
-    echo "Accesso Negato";
+    echo "inserisci query con name, mail e age";
   }
 
 ?>
